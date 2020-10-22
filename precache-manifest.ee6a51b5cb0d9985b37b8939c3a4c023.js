4cb77d40d2916123c84cc995d080042af50d3165
paulocdf <paulo.c.d.figueiredo@gmail.com>
2020-10-22 11:53:17 +0100

    Updates

---
+++ precache-manifest.ee6a51b5cb0d9985b37b8939c3a4c023.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79177825cfc481e33f4e843c64b804a1",
+    "url": "/paulocdf/bookmarks/index.html"
+  },
+  {
+    "revision": "94d0f1d9056b7754581b",
+    "url": "/paulocdf/bookmarks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a048f7b388fae69f113f",
+    "url": "/paulocdf/bookmarks/static/js/2.61012ef4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/paulocdf/bookmarks/static/js/2.61012ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d0f1d9056b7754581b",
+    "url": "/paulocdf/bookmarks/static/js/main.d45eb323.chunk.js"
+  },
+  {
+    "revision": "b707c7b837a9b13f455d",
+    "url": "/paulocdf/bookmarks/static/js/runtime-main.46447def.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paulocdf/bookmarks/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9155671bcdab0239dbe428d38b8e6230.js"
+  "/paulocdf/bookmarks/precache-manifest.ee6a51b5cb0d9985b37b8939c3a4c023.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paulocdf/bookmarks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
