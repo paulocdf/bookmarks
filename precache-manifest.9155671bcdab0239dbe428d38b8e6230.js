8bcb3d4800a364c8de99a04b4be52d0bcf4edff3
paulocdf <paulo.c.d.figueiredo@gmail.com>
2020-10-22 11:51:59 +0100

    Updates

---
+++ precache-manifest.9155671bcdab0239dbe428d38b8e6230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03c5d829e91a159b0443631be1bd6cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d84ebda899d41de2a2ff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a048f7b388fae69f113f",
+    "url": "/static/js/2.61012ef4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.61012ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d84ebda899d41de2a2ff",
+    "url": "/static/js/main.ff6f9c90.chunk.js"
+  },
+  {
+    "revision": "2b05d1bebc36743155ef",
+    "url": "/static/js/runtime-main.997cfdae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
